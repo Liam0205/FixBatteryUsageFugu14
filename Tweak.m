28fa77654b7fb9ec2c1c2ca8a06a88453535845c
Liam Huang <liamhuang0205@gmail.com>
2023-02-16 14:24:09 +0800

    [Tweak.m] setuid does not bypass privelege check, use sudo

--- Tweak.m
+++ Tweak.m
@@ -1,20 +1,18 @@
 #import <Foundation/Foundation.h>
 
-@implementation NSString (FixBatteryUsageFugu14)
+@implementation NSString (Shell)
 
 + (void)load {
   system(
-      "/var/mobile/opt/bin/chflags.page.liam -v noschg,nouchg "
+      "sudo chflags -v noschg,nouchg "
       "/var/mobile/Containers/Data/Fugu14Untether/Library/Caches/com.apple.dyld/*.closure"
-      " && /var/mobile/opt/bin/chown.page.liam 263:263 "
+      " && sudo chown 263:263 "
       "/var/mobile/Containers/Data/Fugu14Untether/Library/Caches/com.apple.dyld/*.closure"
-      " && /var/mobile/opt/bin/chflags.page.liam -v schg,uchg "
+      " && sudo chflags -v schg,uchg "
       "/var/mobile/Containers/Data/Fugu14Untether/Library/Caches/com.apple.dyld/*.closure"
-      " && /var/mobile/opt/bin/chown.page.liam -h 263:263 /var/mobile/Containers/Data/Fugu14Untether/Library"
-      " && /var/mobile/opt/bin/chown.page.liam -R 263:263 /var/db/analyticsd/"
-      " && /var/mobile/opt/bin/launchctl.page.liam stop com.apple.analyticsd"
-      " && sleep 3"
-      " && /var/mobile/opt/bin/launchctl.page.liam start com.apple.analyticsd");
+      " && sudo chown -h 263:263 /var/mobile/Containers/Data/Fugu14Untether/Library"
+      " && sudo chown -R 263:263 /var/db/analyticsd/"
+      " && sudo launchctl stop com.apple.analyticsd && sleep 3 && sudo launchctl start com.apple.analyticsd");
 }
 
 @end
